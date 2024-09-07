dfa7d524fc29a2527a0f13944b6e252ce2efe5f8
dyingsleeper <nik.savkin2004@mail.ru>
2024-09-07 03:05:28 +0300

    deleted useless lines

--- task-2/database.php
+++ task-2/database.php
@@ -1,23 +1,30 @@
 <?php
 try {
-    $db = new PDO('mysql:host=localhost;dbname=task-2','root');
+    $db = new PDO('mysql:host=localhost;dbname=task-2','root', 'root');
 }
 catch (PDOException $e){
     echo $e->getMessage();
     die;
 }
 
-$sql = "delete products from products
+try {
+    $sql = "delete products from products
 LEFT JOIN availabilities on products.id = availabilities.product_id
 where availabilities.product_id IS NULL";
-$stmt = $db->query($sql);
+    $db->query($sql);
 
-$sql = "delete categories from categories
+    $sql = "delete categories from categories
 LEFT JOIN products on products.category_id = categories.id
 where products.category_id IS NULL";
-$stmt = $db->query($sql);
+    $db->query($sql);
 
-$sql = "delete stocks from stocks
+    $sql = "delete stocks from stocks
 left join availabilities on stocks.id = availabilities.stock_id
 where availabilities.stock_id is NULL;";
-$stmt = $db->query($sql);
+    $db->query($sql);
+
+    echo "Successfully deleted";
+}
+catch (Exception $e){
+    echo "Error: " . $e->getMessage();
+}
