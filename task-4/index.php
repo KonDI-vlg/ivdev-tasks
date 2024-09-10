168b9be01464bdf27bb0c2b2d65b45bcfcc78418
dyingsleeper <nik.savkin2004@mail.ru>
2024-09-10 23:37:39 +0300

    полностью начал с нуля, изменил подход. Все работает.

--- task-4/index.php
+++ task-4/index.php
@@ -1,69 +1,37 @@
 <?php
-
 $text = <<<TXT
 <p class="big">
-    Год основания:<b>1589 г.</b> Волгоград отмечает день города в <b>2-е воскресенье сентября</b>. <br>В <b>2023 году</b> эта дата - <b>10 сентября</b>.
+	Год основания:<b>1589 г.</b> Волгоград отмечает день города в <b>2-е воскресенье сентября</b>. <br>В <b>2023 году</b> эта дата - <b>10 сентября</b>.
 </p>
 <p class="float">
-    <img src="https://www.calend.ru/img/content_events/i0/961.jpg" alt="Волгоград" width="300" height="200" itemprop="image">
-    <span class="caption gray">Скульптура «Родина-мать зовет!» входит в число семи чудес России (Фото: Art Konovalov, по лицензии shutterstock.com)</span>
+	<img src="https://www.calend.ru/img/content_events/i0/961.jpg" alt="Волгоград" width="300" height="200" itemprop="image">
+	<span class="caption gray">Скульптура «Родина-мать зовет!» входит в число семи чудес России (Фото: Art Konovalov, по лицензии shutterstock.com)</span>
 </p>
 <p>
-    <i><b>Великая Отечественная война в истории города</b></i></p><p><i>Важнейшей операцией Советской Армии в Великой Отечественной войне стала <a href="https://www.calend.ru/holidays/0/0/1869/">Сталинградская битва</a> (17.07.1942 - 02.02.1943). Целью боевых действий советских войск являлись оборона  Сталинграда и разгром действовавшей на сталинградском направлении группировки противника. Победа советских войск в Сталинградской битве имела решающее значение для победы Советского Союза в Великой Отечественной войне.</i>
+	<i><b>Великая Отечественная война в истории города</b></i></p><p><i>Важнейшей операцией Советской Армии в Великой Отечественной войне стала <a href="https://www.calend.ru/holidays/0/0/1869/">Сталинградская битва</a> (17.07.1942 - 02.02.1943). Целью боевых действий советских войск являлись оборона  Сталинграда и разгром действовавшей на сталинградском направлении группировки противника. Победа советских войск в Сталинградской битве имела решающее значение для победы Советского Союза в Великой Отечественной войне.</i>
 </p>
 TXT;
-function clearTextFromTags($text): string
+
+
+/**
+ * @param string $text Исходный текст
+ * @param int $limit Количество слов, которое нужно оставить
+ * @return string Возвращает обрезанный по количеству слов текст
+ */
+function trimText(string $text, int $limit = 29): string
 {
-    $splitText = [];
-    preg_match_all('/<[^>]+>/i', $text, $allTags, PREG_OFFSET_CAPTURE);
+    $result = [];
 
-    foreach ($allTags[0] as $i => $tag)
-    {
-        if($i + 1 != count($allTags[0]))
-        {
-            $textBetweenTags = substr($text,strlen($tag[0]) + $tag[1], $allTags[0][$i+1][1] - $tag[1] - strlen($tag[0]));
-            $splitText[] = trim($textBetweenTags);
+    $textWithTags = explode(' ', $text);
+    $textWithoutTags = explode(' ', strip_tags($text));
+    $keyWord = $textWithoutTags[$limit - 1];
+    foreach ($textWithTags as $i => $elem) {
+        if(preg_match("/\Q$keyWord\E/u", strip_tags($elem)) && $i >= $limit){
+            break;
         }
+        $result[] = $elem . ' ';
     }
-    $textWithoutTags= implode(" ", $splitText);
-    $indexOfWrongPunctuation = strpos($textWithoutTags, ' . ');
-
-    while ($indexOfWrongPunctuation)
-    {
-        $textWithoutTags = substr($textWithoutTags, 0 , $indexOfWrongPunctuation) . substr($textWithoutTags, $indexOfWrongPunctuation+1, -1);
-        $indexOfWrongPunctuation = strpos($textWithoutTags, ' . ');
-    }
-    return $textWithoutTags;
+    return implode(' ', $result).'...';
 }
 
-$result = [];
-$clearedText = clearTextFromTags($text);
-$splitText = preg_split('/\s+/i', $clearedText);
-preg_match_all('/<[^>]+>/i', $text, $allTags, PREG_OFFSET_CAPTURE);
-
-
-$cntWords = 0;
-foreach ($allTags[0] as $i => $tag){
-    if($i + 1 != count($allTags[0])){
-        $result[] = htmlspecialchars($tag[0]);
-        $lengthBetweenTags = $allTags[0][$i+1][1] - $tag[1] - strlen($tag[0]);
-
-        foreach ($splitText as $word){
-            echo $lengthBetweenTags.' - '.mb_strlen($word).nl2br(PHP_EOL);
-            if($lengthBetweenTags - mb_strlen($word) < -1 || $cntWords == 29){
-                break;
-            }
-            $result[] = $word;
-            if (!preg_match('/^[[:punct:]]$/', $word)){
-                $cntWords++;
-            }
-            array_shift($splitText);
-            $lengthBetweenTags = $lengthBetweenTags - mb_strlen($word);
-        }
-    } else {
-        $result[] = $tag[0];
-    }
-}
-
-$result = implode(" ", $result);
-
+echo trimText($text);
